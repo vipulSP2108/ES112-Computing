bb0014b3b3adf2baa3863b6cb3c21bb84323a8e0
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-03-23 00:03:35 +0530

    Create main.py

---
+++ find largest of four numbers-VIPulSP21/main.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+if(num1 >= num2 and num1 >= num3 and num1 >= num4):
+  print(num1)
+elif(num2 >= num1 and num2 >= num3 and num2 >= num4):
+  print(num2)
+elif(num3 >= num1 and num3 >= num2 and num3 >= num4):
+  print(num3)
+elif(num4 >= num1 and num4 >= num2 and num4 >= num3):
+  print(num4)
